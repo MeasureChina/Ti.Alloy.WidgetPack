488678720476add2c8a4c23bf093a29320fc96a8
metacortex <9ruvie@gmail.com>
2013-10-07 18:44:05 +0900

    app structure

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,109 +1,190 @@
-var drawer = Alloy.createWidget("com.tripvi.navigationDrawer");
-drawer.init($.win, {
-	navigation: [
-		{
-			section: 'default',
-			title: null, // no header view
-			items: [
-				{
-					name: 'setting_about',
-					title: "One",
-					icon: '/appicon.png',
-					window: 'page1',
-					selectedColor: "#844c96",
-				},
-				{
-					name: 'home',
-					title: "Two",
-					icon: '/appicon.png',
-					window: 'page2',
-					selectedColor: "#844c96",
-				},
-				{
-					name: 'person_index',
-					title: "Three",
-					icon: '/appicon.png',
-					window: 'page3',
-					selectedColor: "#844c96",
-				},
-				{
-					name: 'event_index',
-					title: "Click Event",
-					icon: '/appicon.png',
-					onClick: function() {
-						alert(1);
-					},
-				},
-			],
-		},
-	]
-});
+var MODE = "application";
+// var MODE = "test";
 
-drawer.on('menuselect', function(e) {
-	// 변경된 menu 적용
-	if (e.options && e.options.menu) {
-		actionBar.makeActionItems(e.options.menu);
-	} else {
-		actionBar.makeActionItems();
-	}
+// URL Scheme & Data 체크
+// - VIEWER MODE인지 체크
+function checkUrlSchemeAndData() {
+	var activity = Ti.Android.currentActivity;
+	var intent = activity.intent;
+	var data = intent.data;
 	
-	// titleView는 window._options의 title 속성을 우선함
-	if (e.options && e.options.title) {
-		$._lastWindowTitle = e.options.title;
-	} else {
-		$._lastWindowTitle = e.title;
+	// if (data) {
+	// 	var tokens = data.replace("tripvi://", "").split('/');
+	// 	if (tokens.length >= 2) {
+	// 		// 최소 resource name, id의 2개가 존재해야함
+	// 		$._data = data;
+	// 		$._dataTokens = tokens;
+	// 		return true;
+	// 	}
+	// }
+}
+if (checkUrlSchemeAndData()) MODE = "viewer";
+
+
+
+/***********************************************
+  APPLICATION WRAPPER
+***********************************************/
+function openTopMostWindow() {
+	if ($._topMostWindow) return;
+
+	Ti.API.info("\n\n\n\n\n\n\n\n");
+	Ti.API.info(" _______                       __           __       __  __  __  __  ");
+	Ti.API.info("/       \\                     /  |         /  \\     /  |/  |/  |/  |");
+	Ti.API.info("$$$$$$$  |  ______    ______  $$ |   __    $$  \\   /$$ |$$/ $$ |$$ |");
+	Ti.API.info("$$ |__$$ | /      \\  /      \\ $$ |  /  |   $$$  \\ /$$$ |/  |$$ |$$ |");
+	Ti.API.info("$$    $$< /$$$$$$  |/$$$$$$  |$$ |_/$$/    $$$$  /$$$$ |$$ |$$ |$$ | ");
+	Ti.API.info("$$$$$$$  |$$ |  $$ |$$ |  $$ |$$   $$<     $$ $$ $$/$$ |$$ |$$ |$$ | ");
+	Ti.API.info("$$ |__$$ |$$ \\__$$ |$$ \\__$$ |$$$$$$  \\    $$ |$$$/ $$ |$$ |$$ |$$ | ");
+	Ti.API.info("$$    $$/ $$    $$/ $$    $$/ $$ | $$  |   $$ | $/  $$ |$$ |$$ |$$ | ");
+	Ti.API.info("$$$$$$$/   $$$$$$/   $$$$$$/  $$/   $$/    $$/      $$/ $$/ $$/ $$/  ");
+	Ti.API.info("                                         ");
+	Ti.API.info("                                         ");
+
+	Ti.API.info("******************************   APPLICATION OPEN():  as " + MODE);
+	Ti.API.info("******************************                        data  =>  " + $._data);
+
+	$._topMostWindow = Ti.UI.createWindow({ exitOnClose: true, navBarHidden: true, backgroundColor: "transparent" });
+	$._topMostWindow.open();
+}
+
+function closeTopMostWindow() {
+	if ($._topMostWindow) {
+		Ti.API.info("******************************   APPLICATION EXIT()\n\n\n\n");
+		// App 종료
+		Alloy.Globals.application = undefined;
+		$._topMostWindow.close();
 	}
-});
-drawer.on('open', function(e) {
-	// actionbar title 변경
-	$._lastWindowTitle = $._lastWindowTitle || actionBar.getTitle();
-	actionBar.setTitle("MENU");
-	// actions 감추기
-	actionBar.hideActionItems();
-});
-drawer.on('close', function(e) {
-	// actionbar title 복구
-	actionBar.setTitle($._lastWindowTitle || $.win.title);
-	$._lastWindowTitle = undefined;
-	// actions 복구
-	actionBar.showActionItems();
-});
-drawer.on('drawerslide', function(e) {
-	actionBar.onDrawerSlide(e.offset);
-});
-
-Alloy.Globals.app = drawer;
-
-
-var actionBar = Alloy.createWidget("com.tripvi.actionBar");
-actionBar.init($.win, {
-	rootWindow: true,
-	useDrawerMenu: true,
-});
-
-
-$.win.addEventListener("actionbarhome", function() {
-	drawer.toggleLeftDrawer();
-});
-
-
-function onOpenWindow(e) {
-	// default menu
-	drawer.openMenuByName('setting_about');
-}
-function onCloseWindow(e) {
-	$.win.removeEventListener('androidback', onAndroidBack);
-	$.win.removeEventListener('open', onOpenWindow);
-	$.win.removeEventListener('close', onCloseWindow);
-}
-function onAndroidBack(e) {
-	$.win.close();
 }
 
-$.win.addEventListener('androidback', onAndroidBack);
-$.win.addEventListener('open', onOpenWindow);
-$.win.addEventListener('close', onCloseWindow);
+
+
+/***********************************************
+  MAIN APPLICATION WINDOW
+***********************************************/
+exports.openMainWindow = function(options) {
+	$._mainController = Alloy.createController('main');
+	Alloy.Globals.openWindow($._mainController.getView(), options);
+	
+	$._mainController.on('exit', function() {
+		// exit 이벤트가 떨어지면 App 종료
+		Ti.API.info("EXIT signal from:  main");
+		setTimeout(closeTopMostWindow, 200);
+		$._mainController.off();
+	});
+}
+
+exports.closeMainWindow = function() {
+	if ($._mainController) {
+		$._mainController.off();
+		$._mainController.win.close();
+		$._mainController = undefined;
+	}
+}
+
+exports.exit = function() {
+	exports.closeMainWindow();
+	setTimeout(closeTopMostWindow, 200);
+}
+
+exports.openLandingPage = function(options) {
+	// TODO: remove
+	exports.openMainWindow(options);
+	return;
+	
+	var c = Alloy.createController('account/landing', { exit_event: true });
+	Alloy.Globals.openWindow(c.getView(), options);
+	
+	c.on('login', function() {
+		// 로그인이된 경우 main window 오픈
+		exports.openMainWindow();
+		c.off();
+	});
+	
+	c.on('exit', function() {
+		// exit 이벤트가 떨어지면 App 종료
+		Ti.API.info("EXIT signal from:  account/landing");
+		setTimeout(closeTopMostWindow, 200);
+		c.off();
+	});
+}
+
+exports.moveMainPage = function() {
+	if ($ && $._mainController) {
+		$._mainController.menu.openMenuByName('home');
+	}
+}
+
+exports.movePeoplePage = function() { // 명사 메뉴로 이동
+	if ($ && $._mainController) {
+		$._mainController.menu.openMenuByName('person_index');
+	}
+}
+
+exports.movePopularPage = function() { // Top100 메뉴로 이동
+	if ($ && $._mainController) {
+		$._mainController.menu.openMenuByName('popular_index');
+	}
+}
+
+// exports.moveThemePage = function() { // 테마 메뉴로 이동
+// 	if ($ && $._mainController) {
+// 		$._mainController.menu.openMenuByName('home');
+// 	}
+// }
+// 
+// exports.moveSpecialPage = function() { // 스페셜 메뉴로 이동
+// 	if ($ && $._mainController) {
+// 		$._mainController.menu.openMenuByName('home');
+// 	}
+// }
+
+Alloy.Globals.application = exports;
 
 
 
-$.win.open({ animated: false });
+
+/**	
+ *	Booting: Test
+ *
+ */
+if (MODE == 'test') {
+	(function() {
+		
+		openTopMostWindow(); // top most window
+		
+		
+		var c = Alloy.createController('unit_test');
+		c.getView().open();
+		// exitOnClose 에뮬레이트
+		c.win.addEventListener('close', exports.exit);
+
+	})();
+}
+/**	
+ *	Booting: Viewer Mode
+ *
+ */
+else if (MODE == 'viewer') {
+	
+}
+/**	
+ *	Booting: Application
+ *
+ */
+else {
+	(function() {
+		
+		openTopMostWindow(); // top most window
+		
+		// Login되어있으면 바로 MainWindow를 띄운다.
+		var session = Alloy.Globals.getSession();
+		if (session.authenticated) {
+			exports.openMainWindow({ animated: false });
+		} else {
+			// 아니면 landing페이지 오픈한다.
+			exports.openLandingPage({ animated: false });
+		}
+		
+	})();
+}
---
+++ app/controllers/main.js
@@ -0,0 +1,108 @@
+var drawer = Alloy.createWidget("com.tripvi.navigationDrawer");
+drawer.init($.win, {
+	navigation: [
+		{
+			section: 'default',
+			title: null, // no header view
+			items: [
+				{
+					name: 'setting_about',
+					title: "One",
+					icon: '/appicon.png',
+					window: 'page1',
+					selectedColor: "#844c96",
+				},
+				{
+					name: 'home',
+					title: "Two",
+					icon: '/appicon.png',
+					window: 'page2',
+					selectedColor: "#844c96",
+				},
+				{
+					name: 'person_index',
+					title: "Three",
+					icon: '/appicon.png',
+					window: 'page3',
+					selectedColor: "#844c96",
+				},
+				{
+					name: 'event_index',
+					title: "Click Event",
+					icon: '/appicon.png',
+					onClick: function() {
+						alert(1);
+					},
+				},
+			],
+		},
+	]
+});
+
+drawer.on('menuselect', function(e) {
+	// 변경된 menu 적용
+	if (e.options && e.options.menu) {
+		actionBar.makeActionItems(e.options.menu);
+	} else {
+		actionBar.makeActionItems();
+	}
+	
+	// titleView는 window._options의 title 속성을 우선함
+	if (e.options && e.options.title) {
+		$._lastWindowTitle = e.options.title;
+	} else {
+		$._lastWindowTitle = e.title;
+	}
+});
+drawer.on('open', function(e) {
+	// actionbar title 변경
+	$._lastWindowTitle = $._lastWindowTitle || actionBar.getTitle();
+	actionBar.setTitle("MENU");
+	// actions 감추기
+	actionBar.hideActionItems();
+});
+drawer.on('close', function(e) {
+	// actionbar title 복구
+	actionBar.setTitle($._lastWindowTitle || $.win.title);
+	$._lastWindowTitle = undefined;
+	// actions 복구
+	actionBar.showActionItems();
+});
+drawer.on('drawerslide', function(e) {
+	actionBar.onDrawerSlide(e.offset);
+});
+
+
+var actionBar = Alloy.createWidget("com.tripvi.actionBar");
+actionBar.init($.win, {
+	rootWindow: true,
+	useDrawerMenu: true,
+});
+
+
+$.win.addEventListener("actionbarhome", function() {
+	drawer.toggleLeftDrawer();
+});
+
+
+function onOpenWindow(e) {
+	// default menu
+	drawer.openMenuByName('setting_about');
+}
+function onCloseWindow(e) {
+	$.win.removeEventListener('androidback', onAndroidBack);
+	$.win.removeEventListener('open', onOpenWindow);
+	$.win.removeEventListener('close', onCloseWindow);
+}
+function onAndroidBack(e) {
+	$.win.close();
+}
+
+$.win.addEventListener('androidback', onAndroidBack);
+$.win.addEventListener('open', onOpenWindow);
+$.win.addEventListener('close', onCloseWindow);
+
+
+Alloy.Globals.app = drawer;
+
+$.win.open({ animated: false });

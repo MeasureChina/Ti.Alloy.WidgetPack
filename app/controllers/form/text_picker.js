535f020f135309209563311f58a1132ce10eac86
metacortex <9ruvie@gmail.com>
2013-10-28 21:15:30 +0900

    TC-7 #comment textField, textArea는 그냥 getValue(), setValue()를 활용하고 text_picker는 구현

---
+++ app/controllers/form/text_picker.js
@@ -0,0 +1,59 @@
+var args = arguments[0] || {};
+var u = require("utility");
+var F = require("form");
+u.initStyleProperty($.container, args);
+
+
+exports.init = function(options) {
+	// initialize value
+	exports.setValue(options.value || $._items[0][1]);
+}
+
+exports.release = function() {
+	
+}
+
+exports.getValue = function() {
+	return $._value;
+}
+
+exports.setValue = function(val) {
+	$._value = val;
+	// update display
+	updateDisplayAndValue();
+}
+
+function updateDisplayAndValue(text) {
+	if (!text) {
+		text = $._value;
+	} else {
+		$._value = text;
+	}
+	$.valueLabel.text = $._value;
+}
+
+function openPickerWindow() {
+	var win = Ti.UI.createWindow({// open as heavy-weight window
+		windowSoftInputMode: Ti.UI.Android.SOFT_INPUT_ADJUST_RESIZE | Ti.UI.Android.SOFT_INPUT_STATE_ALWAYS_VISIBLE,
+		backgroundColor: "white",
+	});
+	
+	var container = Ti.UI.createView();
+	win.add(container);
+	
+	var textArea = Ti.UI.createTextArea({
+		top: 10,
+		bottom: 10,
+		left: 12,
+		right: 12,
+	});
+	container.add(textArea);
+	
+	// TODO: close button
+	
+	win.addEventListener("close", function(e) {
+		updateDisplayAndValue(textArea.value);
+	});
+	
+	Alloy.Globals.openWindow(win);
+}
